8f8e6b71bf5506971ebbfe1659d3f3c19506d107
JulianDorado0104 <winter.armor@outlook.com>
2025-09-16 21:43:28 -0500

    GetelemetById app.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 // Lista interna donde guardamos los nombres agregados por el usuario.
 const amigos = []
 
+// Referencias a elementos donde capturamos los campos de entrada.
+const inputNombre = document.getElementById('amigo');
+const listaAmigos = document.getElementById('listaAmigos');
+const resultadoSorteo = document.getElementById('resultado');
+
